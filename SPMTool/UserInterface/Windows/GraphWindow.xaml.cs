5e96c37c4c30dde7a74015bd0026ae6ba405aa15
André Mello <andremello91@gmail.com>
2020-12-08 11:10:50 -0400

    Inverted axis config for displacements.

--- SPMTool/UserInterface/Windows/GraphWindow.xaml.cs
+++ SPMTool/UserInterface/Windows/GraphWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Windows;
 using System.Windows.Media;
 using Extensions;
+using Extensions.LinearAlgebra;
 using Extensions.Number;
 using LiveCharts;
 using LiveCharts.Defaults;
@@ -23,23 +24,49 @@ namespace SPMTool.UserInterface
     /// </summary>
     public partial class GraphWindow : Window
     {
-        // Properties
+		/// <summary>
+		/// Get/set the load-displacement <see cref="SeriesCollection"/>.
+		/// </summary>
 		public SeriesCollection LoadDisplacement  { get; set; }
-		private LengthUnit      DisplacementUnit  { get; }
-		public double[]         Displacements     { get; }
-		public double[]         LoadFactors       { get; }
-		public string           DisplacementTitle { get; }
 
-        public GraphWindow(IEnumerable<double> displacements = null, IEnumerable<double> loadFactors = null, LengthUnit displacementUnit = LengthUnit.Millimeter)
+		/// <summary>
+		/// Get the <see cref="LengthUnit"/> of <see cref="Displacements"/>.
+		/// </summary>
+		private LengthUnit DisplacementUnit { get; }
+
+		/// <summary>
+		/// Get the displacements collection.
+		/// </summary>
+		public double[] Displacements { get; }
+
+		/// <summary>
+		/// Get the load factors collection.
+		/// </summary>
+		public double[] LoadFactors { get; }
+
+		/// <summary>
+		/// Get the displacement axis title.
+		/// </summary>
+		public string DisplacementTitle => $"Displacement ({DisplacementUnit.Abbrev()})";
+
+		/// <summary>
+		/// Get/set inverted displacement axis state.
+		/// </summary>
+		private bool Inverted { get; set; }
+
+		/// <summary>
+		/// <see cref="GraphWindow"/> constructor.
+		/// </summary>
+		/// <param name="displacements">The collection of displacements.</param>
+		/// <param name="loadFactors">The collection of load factors.</param>
+		/// <param name="displacementUnit">The <see cref="LengthUnit"/> of <paramref name="displacements"/>.</param>
+		public GraphWindow(IEnumerable<double> displacements = null, IEnumerable<double> loadFactors = null, LengthUnit displacementUnit = LengthUnit.Millimeter)
         {
             InitializeComponent();
 
             Displacements    = displacements.ToArray();
             LoadFactors      = loadFactors.ToArray();
             DisplacementUnit = displacementUnit;
-            DisplacementTitle = $"Displacement ({DisplacementUnit.Abbrev()})";
-
-            var values = GetValues();
 
 			// Initiate series
 			LoadDisplacement = new SeriesCollection
@@ -47,7 +74,7 @@ namespace SPMTool.UserInterface
 				new LineSeries
 				{
 					Title           = "Load Factor x Displacement", 
-					Values          = values, 
+					Values          = GetValues(), 
 					PointGeometry   = null,
 					StrokeThickness = 3,
 					Stroke          = Brushes.LightSkyBlue,
@@ -57,61 +84,61 @@ namespace SPMTool.UserInterface
 				}
 			};
 
+			// Set mapper
+			SetMapper();
+
 			// Set initial point
+			CartesianChart.Series = LoadDisplacement;
             DataContext = this;
         }
 
-		// Add a point to plot
-		public void AddPoint(double displacement, double loadFactor)
-		{
-			LoadDisplacement[0].Values.Add(new ObservablePoint(displacement, loadFactor));
-        }
-
-		// Add a range to plot
-		public void AddRange(double[] displacements, double[] loadFactor)
-		{
-			var points = new ObservablePoint[displacements.Length];
-
-			for (int i = 0; i < displacements.Length; i++)
-				points[i] = new ObservablePoint(displacements[i], loadFactor[i]);
-
-			LoadDisplacement[0].Values.AddRange(points);
-		}
-
-		// Get values to plot
+		/// <summary>
+		/// Get values to plot
+		/// </summary>
 		private ChartValues<ObservablePoint> GetValues()
 		{
 			var values = new ChartValues<ObservablePoint> { new ObservablePoint(0, 0) };
 
-            if (Displacements == null || LoadFactors == null)
+            if (Displacements is null || LoadFactors is null)
 				return values;
-
-			var points = new ObservablePoint[Displacements.Length];
-
-			for (int i = 0; i < Displacements.Length; i++)
-				points[i] = new ObservablePoint(Displacements[i], LoadFactors[i]);
-
-			values.AddRange(points);
+			
+			values.AddRange(Displacements.Zip(LoadFactors, ( d,  l) => new ObservablePoint(d, l)));
 
 			return values;
 		}
 
-		// Get label
+		/// <summary>
+		/// Set mapper for inverting X axis.
+		/// </summary>
+		private void SetMapper()
+		{
+			// If there is a displacement bigger than zero, nothing is done
+			if (Displacements.Any(d => !d.ApproxZero(1E-6) && d > 0))
+			{
+				Inverted = false;
+				return;
+			}
+
+			// Invert x values
+			Inverted = true;
+			LoadDisplacement.Configuration = LiveCharts.Configurations.Mappers.Xy<ObservablePoint>()
+				.X(point => -point.X)
+				.Y(point =>  point.Y);
+
+			// Correct the labels
+			DisplacementAxis.LabelFormatter = x => $"{x * -1}";
+		}
+
+		/// <summary>
+		/// Get the label of a <paramref name="point"/>.
+		/// </summary>
+		/// <param name="point">The <see cref="ChartPoint"/>.</param>
 		private string Label(ChartPoint point)
 		{
 			return
 				$"LF = {point.Y:0.00}\n" +
-				$"u  = {Length.FromMillimeters(point.X).ToUnit(DisplacementUnit)}";
+				$"u  = {Length.FromMillimeters(Inverted ? - point.X : point.X).ToUnit(DisplacementUnit)}";
 		}
-
-        // When a point is clicked
-        private void Chart_OnDataClick(object sender, ChartPoint point)
-        {
-	        //point instance contains many useful information...
-	        //sender is the shape that called the event.
-
-	        MessageBox.Show($"LF: {point.X:0.00} , Displacement: {point.Y:0.00}");
-        }
 
         private void ButtonOK_OnClick(object sender, RoutedEventArgs e)
         {
@@ -120,20 +147,20 @@ namespace SPMTool.UserInterface
 
         private void ButtonExport_OnClick(object sender, RoutedEventArgs e)
         {
-			// Get displacements and loadfactors as vectors
-			var u  = Vector<double>.Build.DenseOfArray(Displacements);
-			var lf = Vector<double>.Build.DenseOfArray(LoadFactors);
+	        // Convert displacements
+			var disp = DisplacementUnit is LengthUnit.Millimeter
+				? Displacements
+				: Displacements.Select(d => d.ConvertFromMillimeter(DisplacementUnit)).ToArray();
 
-			// Convert displacements
-			if (DisplacementUnit != LengthUnit.Millimeter)
-				u = u.Multiply(1.ConvertFromMillimeter(DisplacementUnit));
+			// Get displacements and loadfactors as vectors
+			var u  = disp.ToVector();
+			var lf = LoadFactors.ToVector();
 
 			// Get matrix
 			var result = Matrix<double>.Build.DenseOfColumnVectors(lf, u);
 
 			// Create headers
 			var headers = new[] { "Load Factor", $"Displacement ({DisplacementUnit.Abbrev()})" };
-			var headerList = headers.ToList();
 
 			// Get location and name
 			string
@@ -142,7 +169,7 @@ namespace SPMTool.UserInterface
 				svName = path + name + "_SPMResult.csv";
 
             // Export
-            DelimitedWriter.Write(svName, result, ";", headerList);
+            DelimitedWriter.Write(svName, result, ";", headers);
 
             MessageBox.Show("Data exported to file location.");
         }
