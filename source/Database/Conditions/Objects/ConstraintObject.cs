fd28ea876d1a4a624b86106b5a84b62f6fed8331
André Mello <andremello91@gmail.com>
2021-02-13 15:58:07 -0400

    Added constraints

---
+++ source/Database/Conditions/Objects/ConstraintObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Autodesk.AutoCAD.DatabaseServices;
+using MathNet.Numerics;
+using OnPlaneComponents;
+
+namespace SPMTool.Database.Conditions
+{
+    public class ConstraintObject : ConditionObject<ConstraintObject, Constraint, BlockReference>
+    {
+	    private double RotationAngle =>
+		    Value.Direction switch
+		    {
+				ConstraintDirection.Y => Constants.PiOver2,
+				_                     => 0
+		    };
+
+	    public ConstraintObject(Point position, Constraint value) : base(position, value)
+	    {
+	    }
+
+	    public override BlockReference? CreateEntity() => throw new NotImplementedException();
+    }
+}
